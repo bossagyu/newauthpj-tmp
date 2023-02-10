a02a3a31cf9e758cee7e1b91a8e5bdf3d0616dc0
bossagyu <feel-the-wind.sakura@hotmail.co.jp>
2023-02-10 11:33:57 +0900

    画像を取得してダウンロードできるをことを確認

--- FabricJsSample/app.js
+++ FabricJsSample/app.js
@@ -6,3 +6,13 @@ document.querySelector('#draw').addEventListener("click", function(){
     canvas.freeDrawingBrush.color="black";
     canvas.isDrawingMode = true;
 });
+
+document.querySelector('#postPng').addEventListener("click", function(){
+    const dataURL = canvas.toDataURL({
+        format: "png"
+    });
+    const link = document.createElement("a");
+    link.href = dataURL;
+    link.download = "イラスト.png";
+    link.click();
+});
